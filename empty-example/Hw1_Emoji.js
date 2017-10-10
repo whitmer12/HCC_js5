724953a00625b78e3147f7429e7d3d2cd0e36945
Ryan Whitmer <rywh3896@colorado.edu>
2017-10-09 22:27:38 -0600

    Emoji Done
    EmojiDone

--- empty-example/Hw1_Emoji.js
+++ empty-example/Hw1_Emoji.js
@@ -3,11 +3,14 @@ function setup(){
 
 }
 function draw(){
+  fill(255, 255, 0)
   ellipse(50, 50, 80, 80);
+  fill(255,255,255)
   ellipse(35, 35, 20, 20);
   ellipse(60, 35, 20, 20);
+  fill(0,0,0)
   ellipse(35, 30, 10, 7);
   ellipse(60, 30, 10, 7);
-  line(40, 60, 60, 60);
+  line(40, 65, 55, 65);
 
 }
