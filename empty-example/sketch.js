a51c2f70d724180ce716c74ff3acf39544e28ce7
Ryan Whitmer <rywh3896@colorado.edu>
2017-10-09 22:17:22 -0600

    index.html ready to handle new assignments
    index.html ready to handle new assignments

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -3,11 +3,5 @@ function setup(){
 
 }
 function draw(){
-  ellipse(50, 50, 80, 80);
-  ellipse(35, 35, 20, 20);
-  ellipse(60, 35, 20, 20);
-  ellipse(35, 30, 10, 7);
-  ellipse(60, 30, 10, 7);
-  line(40, 60, 60, 60);
 
 }
